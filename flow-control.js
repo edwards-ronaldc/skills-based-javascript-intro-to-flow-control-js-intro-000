23eeb544a8f95584b557c8e18948fc4ff98be5e3
Ronald C Edwards <rce246@stern.nyu.edu>
2020-01-15 19:45:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -19,6 +19,30 @@ function ternaryTeenager(age) { return age >= 13 && age <= 19 ? "You are a teena
 
 }
 
-function switchAge(age) {
-
+function switchAge(age) {switch (age) {
+  case age === 13:
+  return "You are a teenager";
+  break; 
+  case age === 14:
+  return "You are a teenager";
+  break; 
+  case age === 15:
+  return "You are a teenager";
+  break; 
+  case age === 16:
+  return "You are a teenager";
+  break; 
+  case age === 17:
+  return "You are a teenager";
+  break; 
+  case age === 18:
+  return "You are a teenager";
+  break; 
+  case age === 19:
+  return "You are a teenager";
+  break; 
+  default:
+  return "You have an age"}
 }
+
+

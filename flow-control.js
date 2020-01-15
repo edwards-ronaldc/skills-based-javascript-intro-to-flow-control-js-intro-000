400490e7b20d98d2f468ef242a99f99aee34635a
Ronald C Edwards <rce246@stern.nyu.edu>
2020-01-15 19:39:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,7 +15,7 @@ else {return "You are a grownup"}
 
 }
 
-function ternaryTeenager(age) { age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
+function ternaryTeenager(age) { return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 
 }
 

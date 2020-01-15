c0486083431cc33dc32947c113dad0c7dcbed9f0
Ronald C Edwards <rce246@stern.nyu.edu>
2020-01-15 19:19:00 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,41 @@
-function basicTeenager(age) {
+function basicTeenager(age) { if (age > 12 && age < 19) {
+
+  return "You are a teenager!"}
+
 
 }
 
-function teenager(age) {
+function teenager(age) { if (age > 12 && age < 19) { return "You are a teenager!"}
+else {return "You are not a teenager"}
+}
+
+
+function ageChecker(age) { if(age > 12 && age < 19) {return "You are a teenager!"}
+
+else if (age <= 12) {return "You are a kid"}
+
+else {return "You are a grownup"}
 
 }
 
-function ageChecker(age) {
+function ternaryTeenager(age) {return age > 12 && age < 19 ? "You are a teenager" : "You are not a teenager"
 
 }
 
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
+function switchAge(age)
+{case age === 13: return "You are a teenager";
+break;
+case age === 14: return "You are a teenager";
+break;
+case age === 15: return "You are a teenager";
+break;
+case age === 16: return "You are a teenager";
+break;
+case age === 17: return "You are a teenager";
+break;
+case age === 18: return "You are a teenager";
+break;
+case age === 19: return "You are a teenager";
+break;
+default: return "You have an age"
 }
